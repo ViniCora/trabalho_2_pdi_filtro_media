314fda874c22b5b39e58d8e12894d7038685fbc8
Vinicius Coradassi <viniciuscoradassi@hotmail.com>
2023-09-11 19:17:30 -0300

    Filtro separavel e integral

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import sys
 import timeit
 import cv2
@@ -10,23 +5,68 @@ import numpy as np
 
 ####################################################
 
-INPUT_IMAGE =  'b01 - Original.bmp'
+INPUT_IMAGE =  'a01 - Original.bmp'
 COLORED_IMG = True
-JANELA = 3
+JANELA = 3 ##Uso no codigo apenas a quantidade para somar para a esquerda e para a direta, se o valor aqui estiver igual a 3 então a janela é de tamanho 7 (2*JANELA + 1)
 
 #################################################################
-def filtro_media_separavel(img):
-    return
+def filtro_media_separavel(img, heigth, width):
+    img_out = img.copy()
+    for y in range(0, heigth):
+        primeiro = True
+        guarda_valor = 0
+        for x in range(0 + JANELA, width - JANELA):
+            soma = 0
+            if primeiro:
+                guarda_valor = img[y][x - JANELA]
+                for w in range(x - JANELA, x + JANELA + 1):
+                    soma += img[y][w]
+            else:
+                soma = soma - guarda_valor + img[y][x + JANELA + 1]
+            img_out[y][x] = soma / ((JANELA + 1) * 2)
+
+    img_out2 = img_out.copy()
+    for x in range(0 + JANELA, width - JANELA):
+        for y in range(0 + JANELA, heigth - JANELA):
+            soma = 0
+            for h in range(y - JANELA, y + JANELA + 1):
+                soma += img_out[h][x]
+            img_out2[y][x] = soma / ((JANELA + 1) * 2)
+
+    return img_out2
 
 #################################################################
 
-def filtro_media_integral(img):
-    return
+def calcula_imagem_integral(img, heigth, width):
+    img_out = np.zeros((heigth, width), dtype=float)
+    # Preencha a primeira coluna da imagem de saída
+    img_out[:, 0] = np.cumsum(img[:, 0])
+    # Preencha a primeira linha da imagem de saída
+    img_out[0, :] = np.cumsum(img[0, :])
+    for y in range(1, heigth):
+        for x in range(1, width):
+            valor_saida = img[y, x] + img_out[y - 1, x] + img_out[y, x - 1] - img_out[y - 1, x - 1]
+            img_out[y][x] = valor_saida
+
+    return img_out
+
+def filtro_media_integral(img, heigth, width):
+    img_integral = calcula_imagem_integral(img, heigth, width)
+    img_out = img.copy()
+    for y in range(0, heigth):
+        for x in range(0, width):
+            esquerda = x - JANELA if x - JANELA - 1 >= 0 else 1
+            direita = x + JANELA  if x + JANELA < width else width - 1
+            cima = y - JANELA  if y - JANELA - 1 >= 0 else 1
+            baixo = y + JANELA if y + JANELA < heigth else heigth - 1
+
+            soma = img_integral[baixo][direita] - img_integral[cima - 1][direita] - img_integral[baixo][esquerda - 1] + img_integral[cima - 1][esquerda - 1]
+            img_out[y][x] = soma / ((baixo - cima + 1) * (direita-esquerda+1))
+
+    return img_out
 
 #################################################################
-def filtro_media_ingenuo(img):
-    heigth = img.shape[0]
-    width = img.shape[1]
+def filtro_media_ingenuo(img, heigth, width):
     img_out = img.copy()
     for y in range(0 + JANELA, heigth - JANELA):
         for x in range(0 + JANELA, width - JANELA):
@@ -52,12 +92,16 @@ def main():
         sys.exit()
     canais = cv2.split(img)
     img_canais = []
+    heigth = img.shape[0]
+    width = img.shape[1]
     for canal in canais:
         #cv2.imshow('Canal', canal)
         #cv2.waitKey(0)
         canal = canal.reshape((canal.shape[0], canal.shape[1], 1))
         canal = canal.astype(np.float32) / 255
-        img_canais.append(filtro_media_ingenuo(canal)*255)
+        #img_canais.append(filtro_media_ingenuo(canal, heigth, width)*255)
+        #img_canais.append(filtro_media_integral(canal, heigth, width)*255)
+        img_canais.append(filtro_media_separavel(canal, heigth, width) * 255)
 
     if COLORED_IMG:
         img_out = cv2.merge(img_canais)
@@ -73,5 +117,3 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
